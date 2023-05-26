1ad81d5708c12003234ca3698e46aae28b1ca687
OlivierBey <116079472+OlivierBey@users.noreply.github.com>
2023-05-26 13:15:52 +0200

    26052

--- ParkingLot/src/main/java/parking/controllers/ParkingSpaceController.java
+++ ParkingLot/src/main/java/parking/controllers/ParkingSpaceController.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package parking.controllers;
 
 import java.time.LocalDateTime;
@@ -61,3 +62,62 @@ public class ParkingSpaceController {
 	
 }
 
+=======
+package parking.controllers;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import parking.models.Booking;
+import parking.models.ParkingSpace;
+import parking.services.BookingService;
+import parking.services.ParkingspaceService;
+
+@Controller
+public class ParkingSpaceController {
+
+	@Autowired
+	ParkingspaceService parkingspaceService;
+	@Autowired
+	BookingService bookingService;
+
+	@GetMapping(value = "/startBooking/space")
+	public String getAllparkingSpaces(Model model) {
+		System.out.println("start parking");
+		List<ParkingSpace> parkingSpaces = parkingspaceService.findAllParkingSpaces();
+
+		for (ParkingSpace parkingSpace : parkingSpaces) {
+			
+		}
+		model.addAttribute("parkingSpaces", parkingSpaces);
+		model.addAttribute("findFirstAvailable", parkingspaceService.findFirstAvailable());
+
+		return "parkingspace";
+	}
+
+	@PostMapping("/startBooking/space")
+	public String newBooking(@RequestParam(name = "parkingSpaceId") Integer parkingSpaceId,
+			@RequestParam(name = "bookingId" ) Integer bookingId, Model model) {
+		System.out.println("Confirm " + bookingId + " " + parkingSpaceId);
+		ParkingSpace parkingSpace= parkingspaceService.findParkingspaceById(parkingSpaceId);
+		parkingSpace.setAvailable(false);
+		bookingService.findBookingbyId(bookingId).setParkingSpace(parkingSpace);;
+		
+		model.addAttribute("bookingId", bookingId);
+		model.addAttribute("name", bookingService.findBookingbyId(bookingId).getVehiclePlate());
+		model.addAttribute("parkingSpaceId", parkingspaceService.findParkingspaceById(parkingSpaceId).getLocation());
+		model.addAttribute("startTime", bookingService.findBookingbyId(bookingId).getStarTime());
+		return "confirmBooking.html";
+	}
+
+}
+>>>>>>> Stashed changes
--- ParkingLot/src/main/java/parking/models/ParkingSpace.java
+++ ParkingLot/src/main/java/parking/models/ParkingSpace.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package parking.models;
 
 import org.hibernate.annotations.ColumnDefault;
@@ -41,3 +42,42 @@ public class ParkingSpace {
 	
 	
 }
+=======
+package parking.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_parking_space")
+public class ParkingSpace {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String location;
+	@Column()
+	private boolean available;
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public long getId() {
+		return id;
+	}
+	
+}
+>>>>>>> Stashed changes
